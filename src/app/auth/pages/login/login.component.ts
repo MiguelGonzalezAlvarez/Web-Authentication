db8aa544e9477d8d5991a5b185823fe788e09caf
MiguelMuros <miggonzalv@gmail.com>
2021-11-13 18:25:31 +0100

    feat: create app routes

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class LoginComponent implements OnInit {
 
--- src/app/auth/pages/main/main.component.ts
+++ src/app/auth/pages/main/main.component.ts
@@ -3,8 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class MainComponent implements OnInit {
 
