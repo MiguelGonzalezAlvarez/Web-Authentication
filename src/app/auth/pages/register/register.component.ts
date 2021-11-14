48fd56858db2f97585753c4e99e5161194c4d6c5
MiguelMuros <miggonzalv@gmail.com>
2021-11-14 11:31:32 +0100

    feat: design register screen

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import Swal from 'sweetalert2';
+
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styles: []
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    name: ['Test', [Validators.required]],
+    email: ['test@test.com', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]],
+  });
 
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private authService: AuthService
+  ) { }
+
+
+  registro() {
+    const { name, email, password } = this.miFormulario.value;
+
+    this.authService.registro(name, email, password).subscribe(ok => {
+      if (ok === true) {
+        this.router.navigateByUrl('/dashboard');
+      } else {
+        Swal.fire('Error', ok, 'error');
+      }
+    });
   }
 
 }
