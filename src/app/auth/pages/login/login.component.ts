441c8bf8604eccc642853a2ca48462a15f104ea7
MiguelMuros <miggonzalv@gmail.com>
2021-11-14 11:10:57 +0100

    feat: design login screen and add info txt for the usage of the backend

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styles: []
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    email: ['test1@test.com', [Validators.required, Validators.email]],
+    password: ['123456', [Validators.required, Validators.minLength(6)]],
+  });
 
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+  ) { }
+
+
+  login() {
+
+    const { email, password } = this.miFormulario.value;
+
+    // Aqui hay que logearse
   }
 
 }
